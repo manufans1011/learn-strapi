2219831d8a05f681e640fe834962c4980726ffb7
Moehammad Radif <radif@code.id>
2023-08-19 03:46:40 +0700

    update

--- config/plugins.js
+++ config/plugins.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => ({
           "https://api.vercel.com/v1/integrations/deploy/prj_6hzDuT1wyFmBf4gEYmH1Zzh4v8oE/v2izyhR9lH",
         apiToken: "SD7dUW3EsTBv40AKQv1VH0gn",
         appFilter: "learn-strapi-nu.vercel.app",
-        teamFilter: "",
-        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+        teamFilter: "sitecore-team",
+        roles: ["strapi-super-admin"],
       },
     },
   });
